47d364cfb1f7868b01148b336c5b5f6f421ac8eb
Hem <hpmahimkar@gmail.com>
2022-02-05 19:52:30 +0530

    .
    see

--- comps/navbar.js
+++ comps/navbar.js
@@ -5,18 +5,21 @@ import { useContext } from 'react'
 import AuthContext from '../stores/authContext'
 
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext)
+  const { user, login, logout, authReady } = useContext(AuthContext)
   console.log(user)
 
     return ( 
         <nav>
+         
         <div className="logo">
             <Image src="/TurFIT_Logo.png" width={100} height={90}/>
-            
+         
         </div>
+        
+        { authReady && ( 
+          <ul>
         <Link href="/"><a>Home</a></Link>
-        <a>
-        </a>
+        <a></a>
         
         <Link href="/about"><a>About</a></Link>
         <a></a>
@@ -26,14 +29,17 @@ export default function Navbar() {
         <a></a>
         <a></a>
 
-        <li onClick={login} className="btn">Login/Signup</li>
+        {!user && <li onClick={login} className="btn">Login/Signup</li>}
+        <a></a>
+        {user && <li>{user.email}</li>}
         <a></a>
 
-        <li onClick={logout} className="btn">Logout</li>
+        {user && <li onClick={logout} className="btn">Logout</li>}
 
         <a></a>
         <a></a>
-
+        </ul>
+      )}
         </nav>
      );
 }
