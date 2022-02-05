4e5bca5b9eea50a083ffc80b733a52a02b5d67aa
Hem <hpmahimkar@gmail.com>
2022-02-05 23:35:45 +0530

    final

--- comps/footer.js
+++ comps/footer.js
@@ -4,9 +4,9 @@ import styles from '../styles/footer.module.css'
 const Footer = () => {
     return ( 
         <footer class={styles.footer}>
-            <h3 className={styles.whi}>OUR TEAM  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp;TECH STACK</h3>
-            <h3 className={styles.whi}>HEM MAHIMKAR BT20CSE174 &emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp; Next.js</h3>
-            <h3 className={styles.whi}>KINSHU JAIN BT20CSE116 &emsp;&emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp; Netlify</h3>
+            <h3 className={styles.whi}>&emsp; &emsp;  &emsp; &emsp; OUR TEAM  &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp;&emsp; &emsp;  &emsp; &emsp;&emsp; &emsp;  &emsp; &emsp;TECH STACK</h3>
+            <h3 className={styles.whi}>&emsp; &emsp;  &emsp; &emsp; HEM MAHIMKAR BT20CSE174 &emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp;&emsp; &emsp;  &emsp; &emsp; Next.js</h3>
+            <h3 className={styles.whi}>&emsp; &emsp;  &emsp; &emsp; KINSHU JAIN BT20CSE116 &emsp;&emsp;&emsp;&emsp;&emsp; &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp; &emsp; &emsp;  &emsp; &emsp;&emsp; &emsp;  &emsp; &emsp; Netlify</h3>
     </footer>
      );
 }
